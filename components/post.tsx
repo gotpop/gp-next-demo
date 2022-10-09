7a9604f18d71a412d3ee8d37cf8e588dcb06716e
gotpop <liam@gotpop.net>
2022-10-09 17:16:53 +0100

    Adding dev comments

--- components/post.tsx
+++ components/post.tsx
@@ -11,14 +11,22 @@ interface PostInterface {
     title: string;
     body: string;
   };
-};
+}
 
 export default function PostSingle({ post }: PostInterface) {
   const [showComments, setShowComments] = useState(false);
   const triggerToggle = () => setShowComments(!showComments);
   const url = `https://jsonplaceholder.typicode.com/posts/${post.id}/comments`;
-  
+
   const fetcher = (url: string) => fetch(url).then((res) => res.json());
+
+  // TODO: Switch API to something more sophisticated
+  // This is a good example of why GraphQL can sometimes be required. Here because the only way to fetch
+  // the number of comments is to call the full array of comments, we have no choice but to make the call for each component.
+  // We could call them in getStaticProps but then they wouldn't update.
+  // Ideally we would have the number of comments baked into the post's call
+  // and then make a new call when we actually click on the show comments button
+  // Rather than making 100 calls for 100 posts
   const { data: comments, error } = useSWR(url, fetcher);
 
   return (
@@ -31,14 +39,7 @@ export default function PostSingle({ post }: PostInterface) {
         marginBottom: "16px",
       }}
     >
-      <Paper
-        style={{
-          padding: 20,
-        }}
-        sx={{
-          boxShadow: "rgba(0, 0, 0, 0.08) 1px 1px 9px",
-        }}
-      >
+      <Paper style={{ padding: 20 }} sx={{ boxShadow: "rgba(0, 0, 0, 0.08) 1px 1px 9px" }}>
         <Typography
           variant="h5"
           component="h3"
