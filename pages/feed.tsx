7881168ab38f6aeeb5b31dc9485000e342c8088b
gotpop <liam@gotpop.net>
2022-10-09 17:06:42 +0100

    Adding dev comments

--- pages/feed.tsx
+++ pages/feed.tsx
@@ -3,21 +3,23 @@ import PostSingle from "../components/post";
 import { Box, Container } from "@mui/material";
 import Head from "next/head";
 
+// TODO: Types could be imported
 interface Props {
   posts: [PostInterface];
-}
+};
 
 interface PostInterface {
   id: string;
   title: string;
   body: string;
-}
+};
 
 const Feed: NextPage<Props> = ({ posts }) => {
   return (
     <>
       <Head>
         <title>Feed</title>
+        {/* TODO: Call favicon once at a higher level */}
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Container maxWidth="md">
@@ -33,6 +35,8 @@ const Feed: NextPage<Props> = ({ posts }) => {
 
 export default Feed;
 
+// I use get static props here to take advantage of SSR 
+// Elsewhere I use SWR to call the comments which would be more likley to require realtime updates
 export async function getStaticProps() {
   const url = "https://jsonplaceholder.typicode.com/posts";
 
@@ -44,4 +48,4 @@ export async function getStaticProps() {
       posts: posts,
     },
   };
-}
+};
