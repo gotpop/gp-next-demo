709ebb7bbf934b5e4d7d074fbc888dc1056aadce
gotpop <liam@gotpop.net>
2022-10-17 17:11:14 +0100

    Index changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import NextLink from "next/link";
 import { Paper, Box, Typography, Link, Grid } from "@mui/material";
+import NavBar from "../components/menu";
 
 const Home: NextPage = () => {
   return (
@@ -10,63 +11,7 @@ const Home: NextPage = () => {
         <title>NextJS Demo</title>
       </Head>
       <main>
-        <Box textAlign={"center"} my={8}>
-          <Typography component="h1" variant="h3">
-            NextJS Demo
-          </Typography>
-        </Box>
-        <Grid container spacing={1}>
-          <Grid
-            item
-            xs={6}
-            sx={{
-              display: "flex",
-              justifyContent: "center",
-            }}
-          >
-            <NextLink href="/feed" passHref>
-              <Link>
-                <Paper
-                  sx={{
-                    textAlign: "center",
-                    border: "1px solid grey",
-                    borderRadius: 5,
-                    p: 3,
-                  }}
-                  elevation={3}
-                >
-                  <h2>Go to Feed page</h2>
-                  <p>And start the first task</p>
-                </Paper>
-              </Link>
-            </NextLink>
-          </Grid>
-          <Grid
-            item
-            xs={6}
-            sx={{
-              display: "flex",
-              justifyContent: "center",
-            }}
-          >
-            <NextLink href="/vault" passHref>
-              <Link>
-                <Paper
-                  sx={{
-                    textAlign: "center",
-                    border: "1px solid grey",
-                    borderRadius: 5,
-                    p: 3,
-                  }}
-                  elevation={3}
-                >
-                  <h2>Go to Vault page</h2>
-                  <p>And start the second task</p>
-                </Paper>
-              </Link>
-            </NextLink>
-          </Grid>
-        </Grid>
+        <NavBar />
       </main>
     </div>
   );
